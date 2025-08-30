3d19cae7d154ced08fca54da5743321b11143aa9
dzekiczan1 <p.rzadkowolski@gmail.com>
2025-08-30 18:16:14 +0200

    chore: initial commit

---
+++ client/app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Depthify",
+  description: "ChatGPT powiedział:\n" + "\n" + "Depthify is a modern online logbook for recording your dives. Track your underwater adventures, analyze your statistics, and share your experiences with fellow divers.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ client/app/page.tsx
@@ -0,0 +1,6 @@
+export default function Home() {
+    return (
+        <div className="flex justify-center items-center text-4xl">
+            Depthify
+        </div>);
+}
