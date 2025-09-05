284ebf51e5428e565371dd38521fb56fa87e24ba
dzekiczan1 <p.rzadkowolski@gmail.com>
2025-09-05 19:56:02 +0200

    feat: add loader component

---
+++ client/components/ui/loader.tsx
@@ -0,0 +1,15 @@
+interface LoaderProps {
+  sizeClass?: string;
+  color?: string;
+}
+
+const Loader: React.FC<LoaderProps> = ({ sizeClass = 'h-12 w-12', color = 'text-primary' }) => {
+  return (
+    <div className="flex justify-center items-center w-full">
+      <div
+        className={`animate-spin rounded-full border-t-4 border-b-4 border-blue-500 ${color} ${sizeClass}`}></div>
+    </div>
+  );
+};
+
+export default Loader;
