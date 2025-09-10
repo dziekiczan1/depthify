f569c406d0ce9ad4c9deebe842e88e0703310b37
dzekiczan1 <p.rzadkowolski@gmail.com>
2025-09-10 18:06:27 +0200

    refactor: use Grid component for Features

--- client/components/layout/homepage/features/FeaturesGrid.tsx
+++ client/components/layout/homepage/features/FeaturesGrid.tsx
@@ -1,12 +1,13 @@
 import FeatureCard from './FeatureCard';
 import { featuresData } from '@/lib/homepage/features';
+import { Grid } from '@/components/ui/grid';
 
 export default function FeaturesGrid() {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 mb-16">
+    <Grid className="mb-16">
       {featuresData.map((feature) => (
         <FeatureCard key={feature.id} feature={feature} />
       ))}
-    </div>
+    </Grid>
   );
 }
