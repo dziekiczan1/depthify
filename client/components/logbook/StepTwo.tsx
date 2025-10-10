e0e431cb2454b43db765574baca84fb7abbbec83
dzekiczan1 <p.rzadkowolski@gmail.com>
2025-10-10 20:37:26 +0200

    feat: add steps in logbook add form

---
+++ client/components/logbook/StepTwo.tsx
@@ -0,0 +1,28 @@
+import {
+    FormField,
+    FormItem,
+    FormLabel,
+    FormControl,
+    FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import {UseFormReturn} from "react-hook-form";
+import {DiveFormValues} from "@/components/logbook/logbookFormConfig";
+
+export const StepTwo = ({ form }: { form: UseFormReturn<DiveFormValues> }) => (
+    <>
+        <FormField
+            control={form.control}
+            name="description"
+            render={({ field }) => (
+                <FormItem>
+                    <FormLabel>Description</FormLabel>
+                    <FormControl>
+                        <Input placeholder="Write your dive notes..." {...field} />
+                    </FormControl>
+                    <FormMessage />
+                </FormItem>
+            )}
+        />
+    </>
+);
